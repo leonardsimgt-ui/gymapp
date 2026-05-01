2bcae04a6cf3ad5a443e7890418280d73e61b94d
leonardsimgt-ui <leonard.simgt@gmail.com>
2026-05-01 23:55:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['twilio'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
